72f69568570bc62feab69df589636a98362e8532
varun <varunkumar0930@gmail.com>
2022-12-07 15:47:22 +0530

    sidebar for mobile

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// document.addEventListener("click", function handleClickOutsideBox(event) {
+//   const sidebar = document.getElementById("sidebar");
+//   if (sidebar.classList.contains("active-sidebar")) {
+//     sidebar.classList.remove("active-sidebar");
+//   }
+// });
+
 const makeActive = (id) => {
   const activeButtons = document.querySelectorAll(".active-company-name");
   if (activeButtons.length > 0) {
@@ -30,4 +37,37 @@ const showCompaniesExperience = (id) => {
   console.log(button);
   button.classList.add("active-experience");
   button.classList.remove("experience-company");
+};
+
+const toggleSidebar = () => {
+  const sidebar = document.getElementById("sidebar");
+  if (sidebar.classList.contains("active-sidebar")) {
+    sidebar.classList.remove("active-sidebar");
+  } else {
+    sidebar.classList.add("active-sidebar");
+  }
+
+  const navLine = document.getElementById("nav-line");
+
+  if (navLine.classList.contains("active-nav-line")) {
+    navLine.classList.remove("active-nav-line");
+  } else {
+    navLine.classList.add("active-nav-line");
+  }
+
+  const navLine1 = document.getElementById("nav-line-1");
+
+  if (navLine1.classList.contains("active-nav-line-1")) {
+    navLine1.classList.remove("active-nav-line-1");
+  } else {
+    navLine1.classList.add("active-nav-line-1");
+  }
+
+  const navLine2 = document.getElementById("nav-line-2");
+
+  if (navLine2.classList.contains("active-nav-line-2")) {
+    navLine2.classList.remove("active-nav-line-2");
+  } else {
+    navLine2.classList.add("active-nav-line-2");
+  }
 };
